9e5aa9b39cfcffe8f3aefe1f8a89ca849d4060eb
GillesNC <linggilles@gmail.com>
2026-02-01 18:07:42 +0100

    Add API Yugioh

--- symfony/src/Controller/HomeController.php
+++ symfony/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\YugiohApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(YugiohApiService $yugiohApiService): Response
     {
+        $data = $yugiohApiService->getSet('2-Player Starter Deck: Yuya & Declan');
+        dd($data);
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'data' => $data,
         ]);
     }
 }
---
+++ symfony/src/Service/YugiohApiService.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class YugiohApiService
+{
+    private const API_URL = 'https://db.ygoprodeck.com/api/v7';
+
+    public function __construct(private HttpClientInterface $httpClient) {}
+
+    public function getSingleCard($search)
+    {
+        try {
+            $responseData = $this->httpClient->request('GET', self::API_URL . '/cardinfo.php?name=' . $search);
+            $data = $responseData->toArray();
+
+            $card = $data;
+
+        } catch (\Exception $e) {
+            $error = $e->getMessage();  
+            return $error;
+        }
+        return $card;
+    }
+
+    public function getAllCards($search)
+    {
+        try {
+            $responseData = $this->httpClient->request('GET', self::API_URL . '/cardinfo.php' . $search);
+            $data = $responseData->toArray();
+
+        } catch (\Exception $e) {
+            $error = $e->getMessage();  
+            return $error;
+        }
+        return $data;
+    }
+
+    public function getSet() {
+        try {
+            $responseData = $this->httpClient->request('GET', self::API_URL . '/cardsets.php');
+            $data = $responseData->toArray();
+
+            $sets = $data;
+
+        } catch (\Exception $e) {
+            $error = $e->getMessage();  
+            return $error;
+        }
+        return $sets;
+    }
+
+    public function getRandomCard()
+    {
+        try {
+            $responseData = $this->httpClient->request('GET', self::API_URL . '/randomcard.php');
+            $data = $responseData->toArray();
+
+            $card = $data;
+
+        } catch (\Exception $e) {
+            $error = $e->getMessage();  
+            return $error;
+        }
+        return $card;
+    }
+}
